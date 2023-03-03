e06da2e90392a2dc8a25dfacb0d9b834b673da91
mandieallen <amanda.lanthier@cambriancollege.ca>
2023-03-02 22:16:20 -0500

    Fixed Issue with New-VM creation, & switch array

--- VM-Setup/Step 1-VMCreation.ps1
+++ VM-Setup/Step 1-VMCreation.ps1
@@ -18,8 +18,16 @@ $global:serverISOPath = $null
 $global:win10ISOPath = $null
 
 #create a new internal switch
-function Create-Switch {
+function Add-Switch {
+    #check to see if switch was already created
+    $switch = (get-vmswitch | Where-Object {($_.name -eq "CET1014 Internal Switch") -and  ($_.switchtype -eq "Internal")}).count
+
+    if ($switch -ge 1) { 
+        write-host 'CET1014 Internal Switch Exists Already!' -ForegroundColor green
+    }
+    else {
     New-VMSwitch -Name "CET1014 Internal Switch" -Notes "Internal Switch for CET1014 Virtual Machines" -SwitchType Internal
+    }
 }
 
 #get student number to for VM naming
@@ -47,7 +55,7 @@ function Get-SavePath {
 #get path to downloaded windows server iso
 function Get-ServerISO {
     do {
-    $global:serverISOPath = Read-Host "Enter the path to your Windows Server 2022 standard iso"
+    $global:serverISOPath = Read-Host "Enter the path to your Windows Server 2022 standard iso. Do not include quotes"
     Write-Output $global:serverISOPath
     }
     until (Test-Path $global:serverISOPath)
@@ -56,41 +64,38 @@ function Get-ServerISO {
 #get path to downloaded windows 10 iso
 function Get-WindowsISO {
     do {
-    $global:win10ISOPath = Read-Host "Enter the path to your Windows 10 iso"
+    $global:win10ISOPath = Read-Host "Enter the path to your Windows 10 iso. Do not include Quotes"
     Write-Output $global:win10ISOPath
     }
     until (Test-Path $global:win10ISOPath)
 }
 
 #create hyper-V vm's
-function New-VMCreation ($VMName, $VHDFolder) {
-    #do {
-        #$VmCreation = 
+function New-VMCreation ($VMName, $VHDFolder, $switchName) {
         New-VM -Name $VMName `
-                      -MemoryStartupBytes 2GB `
-                      -BootDevice VHD `
-                      -NewVHDPath $VHDFolder\$VMName.vhdx `
-                      -Path $VHDFolder\VMData `
-                      -NewVHDSizeBytes 40GB `
-                      -Generation 2 `
-                      -Switch "CET1014 Internal Switch" `
-       # $VmCreation                      
-           
-  #  } until (Test-Path $VHDPath)
+        -MemoryStartupBytes 2GB `
+        -BootDevice VHD `
+        -NewVHDPath $VHDFolder\$VMName.vhdx `
+        -Path $VHDFolder\VMData `
+        -NewVHDSizeBytes 40GB `
+        -Generation 2 `
+        -Switch $switchName
 }
 
-Create-Switch
+Add-Switch
 Get-SavePath
 Get-ServerISO
 Get-WindowsISO
 
 #calls create-VM funtion to create VM and adds a dvd drive with path to iso for install
 foreach ($vm in $VMList) {
-    New-VMCreation -VMName $VM -VHDFolder $global:VHDDirectory
+
+    New-VMCreation -VMName $VM -VHDFolder $global:VHDDirectory -switchName (Get-VMSwitch| Where-Object name -eq "CET1014 Internal Switch").name
+    
     if (($vm -eq "$studentNumber-CET1014-DC01") -or ($vm -eq "$studentNumber-CET1014-SVR")) {
     Add-VMDvdDrive -VMName $vm -Path $global:serverISOPath
     }
     elseif ($vm -eq "$studentNumber-CET1014-Win10"){
     Add-VMDvdDrive -VMName $vm -Path $global:win10ISOPath
     }
-    }
+}
